4e95fee601f8175c516a24886bc164b1294b634d
Anubhav Chaurasia <anubhavchaurasia.ac5@gmail.com>
2024-09-26 17:03:24 +0530

    update

--- src/server.js
+++ src/server.js
@@ -2,8 +2,6 @@ const { app } = require(".");
 const { connectDb } = require("./config/db");
 const userService=require("./services/user.service.js")
 
-
-const process.env.PORT=5454;
 app.listen(process.env.PORT,async ()=>{
     await connectDb()
     userService.initializeAdminUser()
